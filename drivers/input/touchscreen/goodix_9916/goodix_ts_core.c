6a721fee201adb2d5b2eccd3f1c55cc38ca8b3fd
AmeChanRain <downloadbot007@gmail.com>
2025-07-04 13:25:13 +0800

    input: touchscreen: goodix_9916: Correct indentations
    Change-Id: I11d974eb15cb3dd50fcc2d40c68dfb874a2c700b

--- drivers/input/touchscreen/goodix_9916/goodix_ts_core.c
+++ drivers/input/touchscreen/goodix_9916/goodix_ts_core.c
@@ -1365,14 +1365,14 @@ finger_pos:
 		input_report_key(dev, BTN_TOUCH, 1);
 		if (global_spi_parent_device != NULL)
 			pm_runtime_set_autosuspend_delay(global_spi_parent_device, 250);
-			pm_runtime_use_autosuspend(global_spi_parent_device);
-			pm_runtime_enable(global_spi_parent_device);
+		pm_runtime_use_autosuspend(global_spi_parent_device);
+		pm_runtime_enable(global_spi_parent_device);
 	} else if (!touch_num && pre_finger_num) {	/*last touch up */
 		input_report_key(dev, BTN_TOUCH, 0);
 		if (global_spi_parent_device != NULL)
 			pm_runtime_set_autosuspend_delay(global_spi_parent_device, 50);
-			pm_runtime_use_autosuspend(global_spi_parent_device);
-			pm_runtime_enable(global_spi_parent_device);
+		pm_runtime_use_autosuspend(global_spi_parent_device);
+		pm_runtime_enable(global_spi_parent_device);
 	}
 	pre_finger_num = touch_num;
 
